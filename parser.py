6a5ac2a3c373274b02e273b987430ca3c8ecfa70
holmrenser <rens.holmer@wur.nl>
2015-11-25 12:09:25 +0100

    parser update

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
 __author__ = 'rensholmer'
 
 from .gff import Gff
-from .gffsubpart import GffSubPart
+from .gffsubpart import GffSubPart,TranslateError
+from itertools import groupby
 
 class Parser(object):
 	"""
@@ -26,7 +27,9 @@ class Parser(object):
 					continue
 				parts = line.strip().split('\t')
 				yield GffSubPart(*parts)
+	#@profile
 	def _ratt(self):
+		print '_ratt()'
 		remove = []
 		featuremap = {}
 		childmap = {}
@@ -39,7 +42,7 @@ class Parser(object):
 				childmap[newID] = []
 
 			if oldID not in featuremap:
-				featuremap[(feature.seqid,oldID)] = newID
+				featuremap.setdefault(newID,set()).add(feature.ID)
 			else:
 				raise ValueError('old ID already exists!')
 
@@ -49,52 +52,84 @@ class Parser(object):
 				newsubID = '{0}.{1}'.format(newID,subfeature.featuretype)
 				if newsubID not in childmap[newID]:
 					childmap[newID].append(newsubID)
-				if oldsubID not in featuremap:
-					featuremap[(subfeature.seqid,oldsubID)] = newsubID
-				elif newsubID != featuremap[oldsubID]:
-					raise ValueError('Old sub ID has multiple new IDs!')
+				featuremap.setdefault(newsubID,set()).add(oldsubID)
+
 				subfeature.ID = newsubID
 				subfeature.attributes['ID'] = [newsubID]
 				subfeature.attributes['Parent'] = [newID]
+				subfeature.source = 'ratt'
 			feature.ID = newID
+			feature.source = 'ratt'
 			feature.attributes['ID'] = [newID]
+			try:
+				del feature.attributes['featflags']
+			except:
+				pass
+			del feature.attributes['locus_tag']
+			del feature.attributes['gene']
+			del feature.attributes['transl_table']
+				
 		for x in featuremap:
-			self.gff.features[x[0]][featuremap[x]] = self.gff.features[x[0]].pop(x[1])
+			for y in featuremap[x]:
+				self.gff.name_index[x] = self.gff.name_index[y]
+			for y in featuremap[x]:
+				self.gff.name_index.pop(y,set())
 		for c in childmap:
 			for t in self.gff[c]:
 				t.children = childmap[c]
 	def _manual(self):
 		pass
-
+	def _remove_noncoding(self):
+		print '_remove_noncoding()'
+		if not self.fasta_file:
+			e = 'Can not remove non coding annotations without sequence data'
+			raise NotImplementedError(e)
+		remove = []
+		for feature in self.gff.getitems(featuretype='mRNA'):
+			try:
+				pep = feature.pep
+			except TranslateError:
+				remove.append(feature.ID)
+				continue
+			if pep[0] != 'M':
+				remove.append(feature.ID)
+			elif pep[-1] != '*':
+				remove.append(feature.ID)
+			elif '*' in pep[1:-1]:
+				remove.append(feature.ID)
+		print 'Remove {0} genes because they do not encode a protein'.format(len(remove))
+		self.gff.remove(remove)
+	def _remove_partial(self):
+		print '_remove_partial()'
+		remove = []
+		for feature in self.gff.getitems(featuretype='mRNA'):
+			if feature.attributes['partial'][0] == 'True':
+				remove.append(feature.ID)
+		print 'Remove {0} genes because they are a partial transfer'.format(len(remove))
+		self.gff.remove(remove)
 	def parse(self):
 		for subpart in self._readlines():
 			self.gff.update(subpart)
 		self.gff.set_children()
 		if self.filetype == 'ratt':
 			self._ratt()
+			#self._remove_partial()
 		if self.fasta_file:
 			self.gff.add_fasta(self.fasta_file)
 			self.gff.getseq(topfeaturetype='mRNA',subfeaturetype='CDS')
 		if self.remove_noncoding:
-			if not self.fasta_file:
-				e = 'Can not remove non coding annotations without sequence data'
-				raise NotImplementedError(e)
-			for feature in self.gff.getitems(featuretype='mRNA'):
-				try:
-					pep = feature.pep
-				except TranslateError:
-					remove.append(feature.ID)
-					continue
-				if pep[0] != 'M':
-					remove.append(feature.ID)
-				elif pep[-1] != '*':
-					remove.append(feature.ID)
-				elif '*' in pep[1:-1]:
-					remove.append(feature.ID)
-			self.gff.remove(remove)
+			self._remove_noncoding()			
 		self.gff.make_index()
 		self.parsed = True
 		return self.gff
+
+def fasta_iter(fasta_file):
+	with open(fasta_file,'rU') as filehandle:
+		faiter = (x[1] for x in groupby(filehandle, lambda line: line[0] == '>'))
+		for header in faiter:
+			header = header.next()[1:].strip()
+			seq = ''.join(s.strip() for s in faiter.next())
+			yield header,seq
 
 def linereader(gff_file,strict=True):
 	"""
