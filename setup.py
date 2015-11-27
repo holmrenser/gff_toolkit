643b48af69aa546f9b369c34028ed49d15967ffa
holmrenser <rens.holmer@wur.nl>
2015-11-27 10:26:59 +0100

    license

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ def main():
 	setup(name='gff_toolkit',
 	      packages = ['gff_toolkit'],
 	      author='rens holmer',
+	      author_email='rens.holmer@wur.nl',
 	      version='0.1.0',
 	      py_modules=['gff','gffsubpart','parser','test','__init__'],
 	      url='https://github.com/holmrenser/gff_toolkit'
