ad59e1295840f8c6ae6132985b3c2105cd39fb3a
holmrenser <rens.holmer@wur.nl>
2015-11-27 10:47:03 +0100

    folder structure

---
+++ gff_toolkit/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'rensholmer'
+
+from test import test
+from parser import parser,linereader,fasta_iter
+from gffsubpart import GffSubPart,TranslateError,SeqError,CoordinateError
+from gff import Gff
