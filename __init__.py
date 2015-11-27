a555d62f7097d53419b0075dca81aef875f39b2d
holmrenser <rens.holmer@wur.nl>
2015-11-27 10:48:03 +0100

    rm old files

--- __init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'rensholmer'
-
-from test import test
-from parser import parser,linereader,fasta_iter
-from gffsubpart import GffSubPart,TranslateError,SeqError,CoordinateError
-from gff import Gff
