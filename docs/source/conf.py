00b640d9bc9481d128742a3697e915eabbfdd97e
holmrenser <rens.holmer@wur.nl>
2016-01-14 12:30:05 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,8 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
